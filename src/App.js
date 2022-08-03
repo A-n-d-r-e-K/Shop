67f7ff83380c3ca35a4a467869ca87090663b8ff
Andrei K <275991@gmail.com>
2022-08-03 14:28:50 +0300

    Adding items to the cart

--- src/App.js
+++ src/App.js
@@ -5,68 +5,77 @@ import Items from "./components/Items";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
-          title: 'Стул серый',
-          img: '1.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
+          title: "Стул серый",
+          img: "1.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
         },
         {
           id: 2,
-          title: 'Стол белый',
-          img: '2.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
+          title: "Стол белый",
+          img: "2.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
         },
         {
           id: 3,
-          title: 'Стол чёрный',
-          img: '3.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
+          title: "Стол чёрный",
+          img: "3.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
         },
         {
           id: 4,
-          title: 'Диван синий',
-          img: '4.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
+          title: "Диван синий",
+          img: "4.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
         },
         {
           id: 5,
-          title: 'Пуфик жёлтый',
-          img: '5.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
+          title: "Пуфик жёлтый",
+          img: "5.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
         },
         {
           id: 6,
-          title: 'Диван зелёный',
-          img: '6.jpeg',
-          desc: 'Rit amet nisl ac est pharetra imperdiet',
-          category: 'chairs',
-          price: '11.46',
-        }
-      ]
-    }
+          title: "Диван зелёный",
+          img: "6.jpeg",
+          desc: "Rit amet nisl ac est pharetra imperdiet",
+          category: "chairs",
+          price: "11.46",
+        },
+      ],
+    };
+    this.addToOrder = this.addToOrder.bind(this);
   }
   render() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Items items={this.state.items}/>
-      <Footer />
-    </div>
-   )
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Footer />
+      </div>
+    );
+  }
+  addToOrder(item) {
+    let isInArray = false;
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) isInArray = true;
+    });
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] });
   }
 }
 
